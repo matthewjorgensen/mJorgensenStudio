dcea984908dcb14666a2bcd7c1598d6c76979fee
Matthew Jorgensen <matthewjorgensen@gmail.com>
2012-10-15 20:04:38 -0700

    refinerycms

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MJorgensenStudio::Application.config.secret_token = '81daf6cdcaf8f4917bbfeb9f45c920480d616225663fcf26fb585fef8c138fe3b1b369a7f9e78aa165923b632688c92cb57def19e41b6624e027bc0ffc3a1d7b'
