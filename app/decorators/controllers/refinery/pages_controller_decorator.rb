1c21bd57bfadbf25a5697557d2ab0b195ad7e90f
Matthew Jorgensen <matthewjorgensen@gmail.com>
2012-11-05 15:10:18 -0700

    added featured galleries to home page

--- app/decorators/controllers/refinery/pages_controller_decorator.rb
+++ app/decorators/controllers/refinery/pages_controller_decorator.rb
@@ -2,8 +2,12 @@ Refinery::PagesController.class_eval do
     # your controller logic goes here
     #homepage
     before_filter :find_all_hero_images, :only => [:home]
+    before_filter :find_featured_portfolio, :only => [:home]
         protected
           def find_all_hero_images
             @homepage_hero_items = Refinery::HeroImages::HeroImage.find(:all, :limit => 4, :order=> "position")
+          end
+          def find_featured_portfolio
+            @featured_portfolios = Refinery::Portfolio::Gallery.find(:all, :limit => 3, :order=>"lft")
           end
   end
